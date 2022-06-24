fbe4995e2d11833e59ebbf122739268eb4cb3afb
8281269367 <ajithkumar.ak95@gmail.com>
2022-06-24 12:21:38 +0530

    added changes and cokmmited

--- onlinestore/onlinestore/settings.py
+++ onlinestore/onlinestore/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'onlinestore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'products', 'templates')
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
