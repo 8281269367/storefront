340088d994391bba86560c92f45304f16eea87cc
8281269367 <ajithkumar.ak95@gmail.com>
2022-06-27 11:57:23 +0530

    removed the dir assignment

--- onlinestore/products/admin.py
+++ onlinestore/products/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Product,Manufacturer
+from .models import Product, Manufacturer
 
 admin.site.register(Product)
 admin.site.register(Manufacturer)
--- onlinestore/products/models.py
+++ onlinestore/products/models.py
@@ -1,27 +1,24 @@
-from pyexpat import model
-from statistics import quantiles
 from django.db import models
-from django.test import modify_settings
 
-# Create your models here.
 class Manufacturer(models.Model):
     name = models.CharField(max_length=120)
     location = models.CharField(max_length=120)
     active = models.BooleanField(default=True)
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
+
 
 class Product(models.Model):
     manufacturer = models.ForeignKey(Manufacturer,
-                                                on_delete=models.CASCADE,
-                                                related_name='products')
+                                     on_delete=models.CASCADE,
+                                     related_name="products")
     name = models.CharField(max_length=120)
-    description = models.TextField(blank=True,null=True)
-    photo = models.ImageField(blank=True,null=True)
+    description = models.TextField(blank=True, null=True)
+    photo = models.ImageField(blank=True, null=True)
     price = models.FloatField()
     shipping_cost = models.FloatField()
     quantity = models.PositiveSmallIntegerField()
 
-    def __str__(self) -> str:
+    def __str__(self):
         return self.name
